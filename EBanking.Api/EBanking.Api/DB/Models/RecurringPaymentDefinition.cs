40cdd65ef1b7a87765e06092748aec0e7a24f5c2
FilipPopescu <filippopescu18@gmail.com>
2026-02-02 00:34:51 +0200

    Added RecurringPaymentDefinition table

---
+++ EBanking.Api/EBanking.Api/DB/Models/RecurringPaymentDefinition.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EBanking.Api.DB.Models;
+
+public enum Recurency
+{
+    Daily,
+    Weekly,
+    Monthly,
+    Yearly
+}
+
+public class RecurringPaymentDefinition(string senderIban, string receiverIban, decimal amount, Recurency recurency,
+    DateTime lastMadePayment, Guid userId)
+{
+    [Key]
+    public int Id { get; private set; }
+
+    public string SenderIban { get; private set; } = senderIban;
+
+    public string ReceiverIban { get; private set; } = receiverIban;
+
+    public decimal Amount { get; private set; } = amount;
+
+    public Recurency Recurency { get; private set; } = recurency;
+
+    public DateTime LastMadePayment { get; set; } = lastMadePayment;
+
+    public Guid UserId { get; private set; } = userId;
+
+    public User User { get; private set; } = null!;
+}
---
+++ EBanking.Api/EBanking.Api/Migrations/20260201223341_AddedRecurringPayments.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EBanking.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedRecurringPayments : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PaymentDefinitions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SenderIban = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReceiverIban = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Amount = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    Recurency = table.Column<int>(type: "int", nullable: false),
+                    LastMadePayment = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PaymentDefinitions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PaymentDefinitions_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PaymentDefinitions_UserId",
+                table: "PaymentDefinitions",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PaymentDefinitions");
+        }
+    }
+}
